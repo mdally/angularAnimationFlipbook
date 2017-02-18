3c1e94ee707dafcca5df6562ed6b2e20cedd813f
Mark Dally <markrdally@gmail.com>
2017-02-18 08:26:16 -0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # angularAnimationFlipbook
+
+by Mark Dally
+
+https://github.com/mdally/angularAnimationFlipbook
+
+================================================
+
 Angular directive for a slider-controlled animation flipbook
+
+###Usage:
+To use, simply link the directive file in your project, along with the css files.
+Then create a folder on your web server that will contain all the slides of the animation. Name them by their frame number (i.e. 1.png - 50.png), with no leading zeros.
+
+```
+<x-animation-flipbook data-title="YourAnimationTitle" data-folder-url="path/to/folder/containing/animation/frames/" data-n-slides="number_of_slides_to_load"/>
+```
+
+###Notes:
+ * Requires jQuery
+ * Looks best with Bootstrap
+ * You can customize the range input css by changing AnimationFlipbook_RangeInput.css. I recommend using http://danielstern.ca/range.css/
+ * I have included a loading gif which is shown while the animation frames are being downloaded. You can change what gif to use in AnimationFlipbook.html
---
+++ dist/directive/AnimationFlipbook.js
@@ -0,0 +1,65 @@
+app.directive('animationFlipbook', function($resource, $q) {
+	return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+        	title: '@',
+        	nSlides: '@',
+        	folderUrl: '@'
+        },
+        templateUrl: 'js/directives/AnimationFlipbook.html',
+        link: function(scope, elem, attrs) {
+			scope.nSlides = parseInt(scope.nSlides);
+			scope.animationSlides = [];
+			scope.currentSlide = 0;
+			scope.allSlidesLoaded = false;
+			scope.showLoadingGif = false;
+
+			scope.slideLoaded = function(slideIdx){
+				scope.animationSlides[slideIdx].loaded = true;
+
+				var allLoaded = true;
+				for(var j = 0; j < scope.nSlides; ++j){
+					if(!scope.animationSlides[j].loaded){
+						allLoaded = false;
+						break;
+					}
+				}
+				if(allLoaded){
+					scope.allSlidesLoaded = true;
+				}
+
+				if(slideIdx == 0){
+					scope.centerLoadingGif(scope.title);
+				}
+			}
+
+			scope.centerLoadingGif = function(title){
+				var elt = $(".animationLoadingGif#"+title);
+
+				elt.css("top", ((elt.parent().height()-elt.height())/2.0).toString()+'px');
+				elt.css("left", ((elt.parent().width()-elt.width())/2.0).toString()+'px');
+				
+				scope.showLoadingGif = true;
+			}
+
+			for(var i = 0; i < scope.nSlides; ++i){
+				scope.animationSlides[i] = {
+					src: scope.folderUrl+(i+1)+'.png',
+					loaded: false
+				};
+			}
+        }
+    };
+});
+
+app.directive('onloadFunction', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            element.bind('load', function() {
+                scope.$apply(attrs.onloadFunction);
+            });
+        }
+    };
+});
